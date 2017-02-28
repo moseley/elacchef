e259f15f97679f0a296fdba1ef2e1a43fb188470
Jeremy Moseley <jeremymoeley@me.com>
2017-02-27 20:53:57 -0800

    Fix variable

--- recipes/data_bags.rb
+++ recipes/data_bags.rb
@@ -1,17 +1,17 @@
 instance = search("aws_opsworks_instance").first
 Chef::Log.info("********** AWS OpsWorks Instance")
 instance.each do |content|
-  Chef::Log.info("********** ${content} **********")
+  Chef::Log.info("********** #{content} **********")
 end
 
 layer = search("aws_opsworks_layer").first
 Chef::Log.info("********** AWS OpsWorks Layer")
 layer.each do |content|
-  Chef::Log.info("********** ${content} **********")
+  Chef::Log.info("********** #{content} **********")
 end
 
 stack = search("aws_opsworks_stack").first
 Chef::Log.info("********** AWS OpsWorks Stack")
 stack.each do |content|
-  Chef::Log.info("********** ${content} **********")
+  Chef::Log.info("********** #{content} **********")
 end
